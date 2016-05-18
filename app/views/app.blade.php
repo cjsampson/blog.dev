9c2391ea571653bc1e5b6d1c042c364e1358455f
CJ Sampson <cjsampson41@gmail.com>
2016-05-18 12:26:51 -0500

    cleaning up the containers on the views and taking out bootstrap

--- app/views/app.blade.php
+++ app/views/app.blade.php
@@ -3,38 +3,33 @@
 <head>
 	<title>Blog</title>
  	@include('partials.head')
-</head> 
+</head>
 <body>
-<div class="siteContainer">
-<?php Session::put('key', 'value'); ?>
-	@if (Session::has('successMessage'))
-	    <div class="alert alert-success">{{{ Session::get('successMessage') }}}</div>
-	@endif
-	@if (Session::has('errorMessage'))
-	    <div class="alert alert-danger">{{{ Session::get('errorMessage') }}}</div>
-	@endif
-	
-	<h3 class="navHeader"><a href="{{{action('MainController@index')}}}">CJ Sampson</a></h3>
-	
-	<div class="fixedSidebar">
-		@include('partials.header')	
-
-		@include('partials.footer')
-	</div> <!-- .fixedSideBar -->
-	
-	
-	{{ Form::open(['method' => 'get', 'action' => 'PostsController@index', 'class' => 'formSearch']) }}
-			{{ Form::input('search', 'q', null, ['placeholder' => 'Search something in particular']) }}
-	{{ Form::close() }} <!-- .formSearch -->
+	<div class="siteContainer">
 		
-	<div class="content">
+	<?php Session::put('key', 'value'); ?>
+
+		<h3 class="navHeader"><a href="{{{action('MainController@index')}}}">CJ Sampson</a></h3>
+
+		<div class="fixedSidebar">
+			@include('partials.header')
+
+			@include('partials.footer')
+		</div> <!-- .fixedSideBar -->
+
+
+		{{ Form::open(['method' => 'get', 'action' => 'PostsController@index', 'class' => 'formSearch']) }}
+				{{ Form::input('search', 'q', null, ['placeholder' => 'Search something in particular']) }}
+		{{ Form::close() }} <!-- .formSearch -->
+
+
 		@yield('content')
-	</div>
+
 
 
 		@include('partials.sideSlide')
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
-		<script src="/js/main.js"></script>					
+		<script src="/js/main.js"></script>
 	</div> <!-- .siteContainer -->
 </body>
 </html>
--- app/views/partials/header.blade.php
+++ app/views/partials/header.blade.php
@@ -1,9 +1,7 @@
-<header>
-	<nav class="blogNav">		
-		<li><a href="{{{action('MainController@about')}}}" class="__blogNav--item">About</a></li>
-		<li><a href="{{{action('MainController@resume')}}}" class="__blogNav--item">Resume</a></li>
-		<li><a href="{{{action('MainController@projects')}}}" class="__blogNav--item">Projects</a></li>
-		<li><a href="{{{action('PostsController@index')}}}" class="__blogNav--item">Blog</a></li>
-		<li><a href="{{{action('MainController@contact')}}}" class="__blogNav--item">Contact</a></li>
-	</nav> <!-- nav.blogNav -->
-</header>
+<nav class="blogNav">		
+	<li><a href="{{{action('MainController@about')}}}" class="__blogNav--item">About</a></li>
+	<li><a href="{{{action('MainController@resume')}}}" class="__blogNav--item">Resume</a></li>
+	<li><a href="{{{action('MainController@projects')}}}" class="__blogNav--item">Projects</a></li>
+	<li><a href="{{{action('PostsController@index')}}}" class="__blogNav--item">Blog</a></li>
+	<li><a href="{{{action('MainController@contact')}}}" class="__blogNav--item">Contact</a></li>
+</nav> <!-- nav.blogNav -->
